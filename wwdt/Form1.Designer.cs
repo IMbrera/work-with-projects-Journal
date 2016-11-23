9849a113e73c9faad28e629027961e9c5b6c3803
Ilin Max <fatgooses5@gmail.com>
2016-11-23 17:00:26 +0300

    regChanges

--- wwdt/Form1.Designer.cs
+++ wwdt/Form1.Designer.cs
@@ -29,11 +29,218 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treyWorker = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.открытьЖурнал = new System.Windows.Forms.ToolStripMenuItem();
+            this.НачатьРаботу = new System.Windows.Forms.ToolStripMenuItem();
+            this.остановитьпауза = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьЗапись = new System.Windows.Forms.ToolStripMenuItem();
+            this.выход = new System.Windows.Forms.ToolStripMenuItem();
+            this.textNotes = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.metroToggle1 = new MetroFramework.Controls.MetroToggle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroToolTip1 = new MetroFramework.Components.MetroToolTip();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treyWorker
+            // 
+            this.treyWorker.ContextMenuStrip = this.contextMenuStrip1;
+            this.treyWorker.Icon = ((System.Drawing.Icon)(resources.GetObject("treyWorker.Icon")));
+            this.treyWorker.Text = "Запись работы проекта";
+            this.treyWorker.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьЖурнал,
+            this.НачатьРаботу,
+            this.остановитьпауза,
+            this.сохранитьЗапись,
+            this.выход});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(174, 114);
+            // 
+            // открытьЖурнал
+            // 
+            this.открытьЖурнал.Image = global::wwdt.Properties.Resources.metrpic;
+            this.открытьЖурнал.Name = "открытьЖурнал";
+            this.открытьЖурнал.Size = new System.Drawing.Size(173, 22);
+            this.открытьЖурнал.Text = "Открыть журнал";
+            this.открытьЖурнал.Click += new System.EventHandler(this.открытьЖурналToolStripMenuItem_Click);
+            // 
+            // НачатьРаботу
+            // 
+            this.НачатьРаботу.Image = global::wwdt.Properties.Resources.start;
+            this.НачатьРаботу.Name = "НачатьРаботу";
+            this.НачатьРаботу.Size = new System.Drawing.Size(173, 22);
+            this.НачатьРаботу.Text = "Начать работу";
+            this.НачатьРаботу.Click += new System.EventHandler(this.Начать_работу);
+            // 
+            // остановитьпауза
+            // 
+            this.остановитьпауза.Image = global::wwdt.Properties.Resources.green_pause;
+            this.остановитьпауза.Name = "остановитьпауза";
+            this.остановитьпауза.Size = new System.Drawing.Size(173, 22);
+            this.остановитьпауза.Text = "Остановить/пауза";
+            this.остановитьпауза.Click += new System.EventHandler(this.остановитьпаузаToolStripMenuItem_Click);
+            // 
+            // сохранитьЗапись
+            // 
+            this.сохранитьЗапись.Image = global::wwdt.Properties.Resources.notepad;
+            this.сохранитьЗапись.Name = "сохранитьЗапись";
+            this.сохранитьЗапись.Size = new System.Drawing.Size(173, 22);
+            this.сохранитьЗапись.Text = "Сохранить запись";
+            this.сохранитьЗапись.Click += new System.EventHandler(this.сохранитьЗаписьToolStripMenuItem1_Click);
+            // 
+            // выход
+            // 
+            this.выход.Image = global::wwdt.Properties.Resources.cerrar;
+            this.выход.Name = "выход";
+            this.выход.Size = new System.Drawing.Size(173, 22);
+            this.выход.Text = "Выход";
+            this.выход.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // textNotes
+            // 
+            this.textNotes.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textNotes.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textNotes.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textNotes.Location = new System.Drawing.Point(0, 186);
+            this.textNotes.Multiline = true;
+            this.textNotes.Name = "textNotes";
+            this.textNotes.Size = new System.Drawing.Size(346, 53);
+            this.textNotes.TabIndex = 30;
+            this.textNotes.Text = "Описание действий";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(345, 164);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.metroTile1);
+            this.panel1.Controls.Add(this.metroToggle1);
+            this.panel1.Controls.Add(this.labelTime);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 166);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(346, 20);
+            this.panel1.TabIndex = 32;
+            // 
+            // metroToggle1
+            // 
+            this.metroToggle1.AutoSize = true;
+            this.metroToggle1.Checked = true;
+            this.metroToggle1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.metroToggle1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.metroToggle1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.metroToggle1.FontSize = MetroFramework.MetroLinkSize.Medium;
+            this.metroToggle1.Location = new System.Drawing.Point(266, 0);
+            this.metroToggle1.Name = "metroToggle1";
+            this.metroToggle1.Size = new System.Drawing.Size(80, 20);
+            this.metroToggle1.Style = MetroFramework.MetroColorStyle.Black;
+            this.metroToggle1.TabIndex = 34;
+            this.metroToggle1.Text = "On";
+            this.metroToggle1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroToggle1.UseSelectable = true;
+            this.metroToggle1.CheckedChanged += new System.EventHandler(this.metroToggle1_CheckedChanged_2);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Время работы:";
+            // 
+            // labelTime
+            // 
+            this.labelTime.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelTime.Enabled = false;
+            this.labelTime.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(116, 0);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(74, 20);
+            this.labelTime.TabIndex = 33;
+            this.labelTime.Text = "00:00:00";
+            this.labelTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.metroTile1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.metroTile1.Location = new System.Drawing.Point(185, 0);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(81, 20);
+            this.metroTile1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTile1.TabIndex = 35;
+            this.metroToolTip1.SetToolTip(this.metroTile1, "SaveChanges");
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // metroToolTip1
+            // 
+            this.metroToolTip1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroToolTip1.StyleManager = null;
+            this.metroToolTip1.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(346, 239);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textNotes);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Журнал";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.NotifyIcon treyWorker;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьЖурнал;
+        private System.Windows.Forms.ToolStripMenuItem НачатьРаботу;
+        private System.Windows.Forms.ToolStripMenuItem остановитьпауза;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьЗапись;
+        private System.Windows.Forms.ToolStripMenuItem выход;
+        private System.Windows.Forms.TextBox textNotes;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Components.MetroToolTip metroToolTip1;
+        private MetroFramework.Controls.MetroToggle metroToggle1;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- wwdt/Form1.cs
+++ wwdt/Form1.cs
@@ -2,19 +2,199 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace wwdt
 {
     public partial class Form1 : Form
     {
+              DateTime start;
+        Timer timer = new Timer();
+        private DataGridView dgv;
+        private SqlConnection conn;
+        private DataSet dataS;
+        private SqlDataAdapter adap;
+        SqlCommand command = new SqlCommand();
         public Form1()
         {
+
             InitializeComponent();
+
+            //connect
+            #region
+            conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Desktop\Диплом\wwdt\wwdt\db.mdf;Integrated Security=True");
+            adap = new SqlDataAdapter();
+            dataS = new DataSet();
+            //    dataGridView1.DataSource = tableBindingSource;
+            adap.SelectCommand = new SqlCommand("select * from dbNET", conn);
+
+            adap.InsertCommand = new SqlCommand("Insert into dbNET (Start, Описание, ВремяРаботы) values (@id, @dw, @sw)", conn);
+
+            adap.InsertCommand.Parameters.Add("@id", SqlDbType.Char, 20, "Start");
+            adap.InsertCommand.Parameters.Add("@dw", SqlDbType.Char, 100, "Описание");
+            adap.InsertCommand.Parameters.Add("@sw", SqlDbType.Char, 10, "ВремяРаботы");
+
+
+            adap.UpdateCommand = new SqlCommand("update dbNET set Start=@id, Описание=@dt, ВремяРаботы = @sw", conn);
+            adap.UpdateCommand.Parameters.Add("@id", SqlDbType.Char, 20, "Start");
+            adap.UpdateCommand.Parameters.Add("@dw", SqlDbType.Char, 100, "Описание");
+            adap.UpdateCommand.Parameters.Add("@sw", SqlDbType.Char, 10, "ВремяРаботы");
+
+
+            adap.DeleteCommand = new SqlCommand("delete from dbNET where Start=@id", conn);
+            adap.DeleteCommand.Parameters.Add("@id", SqlDbType.Char, 20, "Start");
+            adap.Fill(dataS, "dbNET");
+            /*
+            try
+            {
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            */
+            // dataGridView1.DataSource = dataS.Tables[0];
+            dgv = new DataGridView { Parent = this, Dock = DockStyle.Top, DataSource = dataS.Tables["dbNET"] };
+            dataGridView1.DataSource = dataS.Tables["dbNET"];
+            #endregion
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Tick += new EventHandler(timer_Tick);
+            остановитьпауза.Enabled = false;
+            сохранитьЗапись.Enabled = false;
+        }
+    
+
+
+        private void StartTimer()
+        {
+            
+            start = DateTime.Now;
+            timer.Interval = 1000; //1 sec
+            timer.Start();
+            сохранитьЗапись.Enabled = false;
+            остановитьпауза.Enabled = true;
+            выход.Enabled = false;
+
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+
+            TimeSpan time = (DateTime.Now - start).Duration();
+            labelTime.Text = time.ToString(@"hh\:mm\:ss");
+            // labelTime.Text = (++timeCoStarter).ToString();
+
+
+        }
+
+
+
+        private void addNotetoGrid()
+        {
+            dataS.Tables["dbNET"].Rows.Add(start.ToString("yyyy-MM-dd HH:mm"), textNotes.Text, labelTime.Text);
+
+
+            /*
+        int row = dataGridView1.Rows.Count;
+        adap.UpdateCommand.Parameters.Add(start.ToString("yyyy-MM-dd HH:mm"));
+
+        dataGridView1["ВремяРаботы", row].Value = labelTime.Text;
+        dataGridView1["Описание", row].Value = textNotes.Text;
+        */
+        }
+
+        private void endtime_Click(object sender, EventArgs e)
+        {
+            
+            timer.Stop();
+            addNotetoGrid();
+            textNotes.Text = "";
+            labelTime.Text = "00:00:00";
+
+        }
+
+        private void открытьЖурналToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Visible = true;
+            ShowInTaskbar = true;           
+            // form.Show();
+        }
+
+        private void metroToggle1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (metroToggle1.Checked == false)
+            {
+                WindowState = FormWindowState.Minimized;
+                ShowInTaskbar = false;
+                treyWorker.Visible = true;
+            }
+        }
+
+           private void сохранитьЗаписьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            adap.Update(dataS.Tables["dbNet"]);
+            выход.Enabled = true;
+            НачатьРаботу.Enabled = true;
+            сохранитьЗапись.Enabled = false;
+        }
+
+        private void остановитьпаузаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textNotes.Text=="")
+            {
+               if (DialogResult==DialogResult.Yes)
+                
+                    textNotes.Focus();
+                
+
+            }
+            сохранитьЗапись.Enabled = true;
+            НачатьРаботу.Enabled = false;
+            выход.Enabled = false;
+            timer.Stop();
+            addNotetoGrid();
+            textNotes.Text = "";
+            labelTime.Text = "00:00:00";
+            остановитьпауза.Enabled = false;
+            НачатьРаботу.Enabled = true;
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Начать_работу(object sender, EventArgs e)
+        {
+            StartTimer();
+            НачатьРаботу.Enabled = false;
+            textNotes.Text = "";
+        }
+
+      
+
+        private void metroToggle1_CheckedChanged_2(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+            adap.Update(dataS.Tables["dbNET"]);
+            выход.Enabled = true;
+        }
+        
     }
 }
